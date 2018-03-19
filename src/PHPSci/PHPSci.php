d5aad7a601ac9a83acc198e8b6d80845110586bf
Henrique Borba <henrisaviatto@gmail.com>
2018-03-19 02:00:17 -0300

    Implemented Base Exception, Dummy Logger, Missing Extension Exception and core PHPSci Object and CArray abstraction layer.

--- src/PHPSci/PHPSci.php
+++ src/PHPSci/PHPSci.php
@@ -1,6 +1,36 @@
 <?php
 namespace PHPSci;
 
-class PHPSci {
+use PHPSci\Backend\CArray;
+use PHPSci\Backend\Exceptions\ExtensionMissingException;
+use PHPSci\Core\Initializable;
+use PHPSci\Core\LinearAlgebra;
+use PHPSci\Core\Randomizable;
+
+/**
+ * Main PHPSci Object
+ *
+ * @package PHPSci
+ */
+class PHPSci extends CArray {
+
+    use LinearAlgebra, Randomizable, Initializable;
+
+    /**
+     * PHPSci constructor.
+     *
+     * @param array $input
+     * @param int $flags
+     * @param string $iterator_class
+     * @throws ExtensionMissingException
+     */
+    public function __construct($input = array(), int $flags = 0, string $iterator_class = "ArrayIterator")
+    {
+        if (!extension_loaded('phpsci')) {
+            throw new ExtensionMissingException("PHPSci Extension is required. Download it here: https://github.com/phpsci/phpsci");
+        }
+        parent::__construct($input, $flags, $iterator_class);
+    }
+
 
 }
