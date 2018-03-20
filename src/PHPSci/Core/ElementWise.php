3fc9572cf6419884c2a8747500139065feadd4f2
Henrique Borba <henrisaviatto@gmail.com>
2018-03-20 02:02:55 -0300

    Updated ElementWise trait

---
+++ src/PHPSci/Core/ElementWise.php
@@ -0,0 +1,28 @@
+<?php
+namespace PHPSci\Core;
+
+use PHPSci\Backend\CArray;
+use PHPSci\PHPSci;
+
+/**
+ * Handle ElementWise Operations on CArrays
+ *
+ * @author Henrique Borba <henrique.borba.dev@gmail.com>
+ * @package PHPSci\Core
+ */
+trait ElementWise
+{
+
+    /**
+     * Sum of array elements over a given axis.
+     *
+     * @author Henrique Borba <henrique.borba.dev@gmail.com>
+     * @param CArray $a
+     * @return PHPSci
+     * @throws \PHPSci\Backend\Exceptions\ExtensionMissingException
+     */
+    public static function sum(CArray $a) {
+        return new PHPSci(\CPHPSci::sum($a->toArray(), 0));
+    }
+
+}
