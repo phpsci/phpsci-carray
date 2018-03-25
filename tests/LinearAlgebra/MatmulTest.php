362ba44a58b72ec372f3d5ba4b7fce8d75983cb1
Henrique Borba <henrisaviatto@gmail.com>
2018-03-25 16:37:05 -0300

    Implemented matmul test with square matrices.

--- tests/LinearAlgebra/MatmulTest.php
+++ tests/LinearAlgebra/MatmulTest.php
@@ -1,9 +1,31 @@
 <?php
-
+namespace PHPSci\Tests\LinearAlgebra;
 
 use PHPUnit\Framework\TestCase;
+use PHPSci\PHPSci as ps;
 
+/**
+ * Class MatmulTest
+ *
+ * @author Henrique Borba <henrique.borba.dev@gmail.com>
+ * @package PHPSci\Tests\LinearAlgebra
+ *
+ */
 class MatmulTest extends TestCase
 {
+
+    /**
+     *
+     */
+    public function testMatmul() {
+        $expected = [
+            [58, 64],
+            [139, 154]
+        ];
+        $a = ps::fromArray([[1,2,3],[4,5,6]]);
+        $b = ps::fromArray([[7,8],[9,10],[11,12]]);
+        $returned = ps::matmul($a, $b);
+        $this->assertEquals($expected, $returned->toArray());
+    }
 
 }
