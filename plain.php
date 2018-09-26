ba8c87acb71d04000b01f9ead79a71f376f9cc62
Henrique Borba <henrique.borba.dev@gmail.com>
2018-09-26 01:31:57 -0300

    Removed plain.php test file.

--- plain.php
+++ plain.php
@@ -2,6 +2,34 @@
 require_once "vendor/autoload.php";
 
 use PHPSci\PHPSci as ps;
+use PHPSci\CArray as ca;
+use PHPSci\Plot\Plotter as plt;
 
-$a = ps::fromArray([[1, 2, 3], [4, 5, 6]]);
-echo ps::standard_normal(10);
+
+$data = ca::fromArray([
+    [0 ,0],
+    [1, 1],
+    [0, 1],
+    [1, 0]
+
+]);
+
+$labels = ca::fromArray([
+    0, 1, 0, 0
+]);
+
+# var = mean(abs(x - x.mean())**2).
+
+# echo ca::mean($data, 0);
+# echo ca::mean(ca::square(ca::abs(ca::subtract($data, ca::mean($data, 0)))),0);
+
+
+$classifier = new \PHPSci\NaiveBayes\GaussianNB();
+
+
+$classifier->fit($data, $labels);
+$prediction = $classifier->predict($data);
+
+print_r($prediction);
+
+//print_r($prediction);
