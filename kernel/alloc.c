18b13485d9d8b9137bbc1a5708c47a54114631fd
Henrique Borba <henrique.borba.dev@gmail.com>
2020-11-08 22:02:18 -0300

    Added CARRAY_GC_DEBUG environ variable for Garbage Collection debugging

--- kernel/alloc.c
+++ kernel/alloc.c
@@ -2,6 +2,22 @@
 #include "carray.h"
 #include "buffer.h"
 
+/**
+ * If CARRAY_GC_DEBUG env is True, CArray Garbage Collector
+ * will print debug messages when destructing objects.
+ */
+static int
+CArrayGC_ISDEBUGON()
+{
+    if (getenv("CARRAY_GC_DEBUG") == NULL) {
+        return 0;
+    }
+    if (!strcmp(getenv("CARRAY_GC_DEBUG"), "0")) {
+        return 0;
+    }
+    return 1;
+}
+
 void
 CArrayDescriptor_FREE(CArrayDescriptor * descr)
 {
@@ -31,9 +47,9 @@ CArray_DECREF(CArray *target)
 }
 
 /**
- * Alocates CArray Data Buffer based on numElements and elsize from 
+ * Alocates CArray Data Buffer based on numElements and elsize from
  * CArray descriptor.
- **/ 
+ **/
 void
 CArray_Data_alloc(CArray * ca)
 {
@@ -83,11 +99,6 @@ CArrayDescriptor_DECREF(CArrayDescriptor * descriptor)
 void
 CArray_Free(CArray * self)
 {
-    if (self->descriptor != NULL) {
-        if (self->descriptor->refcount <= 0) {
-            CArrayDescriptor_FREE(self->descriptor);
-        }
-    }
     if(self->refcount <= 0) {
         efree(self->dimensions);
         efree(self->strides);
@@ -102,15 +113,17 @@ CArray_Free(CArray * self)
 
 /**
  * Free CArrays owning data buffer
- */  
+ */
 void
 _free_data_owner(MemoryPointer * ptr)
 {
     CArray * array = CArray_FromMemoryPointer(ptr);
-
     CArrayDescriptor_DECREF(array->descriptor);
 
     if(array->descriptor->refcount < 0) {
+        if (CArrayGC_ISDEBUGON()) {
+          php_printf("\n[CARRAY_GC_DEBUG][DESCR] Freeing Descriptor from CArray ID %d", ptr->uuid);
+        }
         CArrayDescriptor_FREE(CArray_DESCR(array));
     }
 
@@ -118,6 +131,9 @@ _free_data_owner(MemoryPointer * ptr)
     efree(array->dimensions);
     efree(array->strides);
     if(array->refcount < 0) {
+        if (CArrayGC_ISDEBUGON()) {
+          php_printf("\n[CARRAY_GC_DEBUG] Freeing DATA from CArray ID %d", ptr->uuid);
+        }
         efree(array->data);
         buffer_remove(ptr);
     }
@@ -125,38 +141,50 @@ _free_data_owner(MemoryPointer * ptr)
 
 /**
  * Free CArrays that refers others CArrays
- */  
+ */
 void
 _free_data_ref(MemoryPointer * ptr)
 {
     MemoryPointer tmp;
-    
+
     CArray * array = CArray_FromMemoryPointer(ptr);
-    
+
     CArray_DECREF(array);
     CArray_DECREF(array->base);
     CArrayDescriptor_DECREF(array->descriptor);
     if(array->descriptor->refcount < 0) {
+        if (CArrayGC_ISDEBUGON()) {
+          php_printf("\n[CARRAY_GC_DEBUG][DESCR][VIEW] Freeing Descriptor from CArray ID %d", ptr->uuid);
+        }
         CArrayDescriptor_FREE(CArray_DESCR(array));
     }
     efree(array->dimensions);
     efree(array->strides);
-    
+
     if(array->refcount < 0 && array->base->refcount < 0) {
+        if (CArrayGC_ISDEBUGON()) {
+          php_printf("\n[CARRAY_GC_DEBUG][VIEW] Freeing CArray ID %d", ptr->uuid);
+        }
         if(CArray_CHKFLAGS(array->base, CARRAY_ARRAY_OWNDATA)) {
+            if (CArrayGC_ISDEBUGON()) {
+              php_printf("\n[CARRAY_GC_DEBUG][VIEW] Freeing DATA from CArray ID %d", ptr->uuid);
+            }
             efree(array->data);
-        } 
+        }
         tmp.uuid = array->base->uuid;
         buffer_remove(&tmp);
-    }   
+    }
     if(array->refcount < 0){
+        if (CArrayGC_ISDEBUGON()) {
+          php_printf("\n[CARRAY_GC_DEBUG][VIEW] Freeing CArray ID %d", ptr->uuid);
+        }
         buffer_remove(ptr);
-    }    
+    }
 }
 
 /**
  * Free CArray using MemoryPointer
- **/ 
+ **/
 void
 CArray_Alloc_FreeFromMemoryPointer(MemoryPointer * ptr)
 {
